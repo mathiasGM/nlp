d42b8bafe7f07edc1e714650532ba99adac91ac2
Mathias Giovanni Møller <33517196+mathiasGM@users.noreply.github.com>
2021-04-16 12:12:28 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,51 @@
+import re, nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('stopwords')
+
+
+def lemmatize(text):
+  lemmatizer = WordNetLemmatizer()
+  tokens = word_tokenize(text)
+
+  sentence_lemmatized = ""
+
+  for token in tokens:
+    token_lemmatized = lemmatizer.lemmatize(token)    
+    sentence_lemmatized = sentence_lemmatized + " " + token_lemmatized
+  
+  return sentence_lemmatized.strip().lower()
+
+
+def stem(text):
+  stemmer = PorterStemmer()
+  tokens = word_tokenize(text)
+
+  sentence_stemmed = ""
+
+  for token in tokens:
+    token_stemmed = stemmer.stem(token)
+    sentence_stemmed = sentence_stemmed + " " + token_stemmed
+  
+  return sentence_stemmed.strip().lower()
+
+
+def remove_stopwords(text):
+  stop_words = set(stopwords.words('english')) 
+
+  word_tokens = word_tokenize(text)
+
+  filtered_sentence = [w for w in word_tokens if not w in stop_words]
+
+  sentence_no_stopwords = " ".join(filtered_sentence)
+  
+  return sentence_no_stopwords.strip()
+
+
 def string_to_chunks(string_input, n):
     string_split = string_input.split(" ")
     
